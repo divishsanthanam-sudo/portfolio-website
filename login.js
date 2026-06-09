bb1228a426598d9ffb319840e877c7e10541fff3
divishsanthanam-sudo <divishsanthanam@gmail.com>
2026-06-10 02:34:15 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,16 @@
+function login(){
+
+let user =
+document.getElementById("username").value;
+
+let pass =
+document.getElementById("password").value;
+
+if(user === "admin" && pass === "1234"){
+window.location.href = "admin.html";
+}
+else{
+alert("Invalid Login");
+}
+
+}
