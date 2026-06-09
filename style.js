51efdddd97e37e11fd06d49936afd0a97cf1da74
divishsanthanam-sudo <divishsanthanam@gmail.com>
2026-06-10 02:31:54 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,48 @@
+ body{
+font-family:Arial;
+padding:20px;
+background:#f4f4f4;
+}
+
+nav{
+background:#007bff;
+padding:15px;
+}
+
+nav a{
+color:white;
+text-decoration:none;
+margin-right:15px;
+}
+
+section{
+margin-top:50px;
+}
+
+input,textarea{
+width:100%;
+padding:10px;
+margin:10px 0;
+}
+
+button{
+background:#007bff;
+color:white;
+padding:10px;
+border:none;
+cursor:pointer;
+}
+
+.login-box{
+width:300px;
+margin:auto;
+margin-top:100px;
+}
+
+.card{
+background:white;
+padding:15px;
+margin:10px;
+border-radius:10px;
+box-shadow:0 0 5px gray;
+}
